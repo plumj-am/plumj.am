e118b3c3e33c8f950ea0dce5998f813eef63c8f4
PlumJam <git@plumj.am>
2025-09-26 22:47:42 +0200

    normal.layout: Ensure blog/slug matches blog route.
    Necessary to ensure blog route is highlighted in navigation when on any
    individual post.
    
    I need to more thoroughly check the docs/crates to see if there's a
    better way.

--- normal/layout.rs
+++ normal/layout.rs
@@ -44,20 +44,30 @@ pub fn Layout() -> Element {
 fn Navbar() -> Element {
     let curr_route = use_route::<Route>();
 
+    let check_route_active = |nav_route: &Route| -> bool {
+        match (nav_route, &curr_route) {
+            (&Route::Blog {}, &Route::Blog {} | &Route::BlogPost { .. }) => true,
+            _ => nav_route == &curr_route,
+        }
+    };
+
     rsx! {
         div { class: "h-16 border-l-1 border-b-1 border-fg w-full max-w-5xl mx-auto",
             div { class: "grid grid-cols-4 w-full h-full text-2xl tracking",
                 for link in NAV_LINKS {
-                    div {
-                        Link { class: "group flex items-center justify-center w-full h-full border-fg hover:bg-[var(--color-hover)] transition-scale duration-100 hover:scale-120 hover:shadow-[inset_0_0_0_1px_var(--color-fg)] hover:border-0 border-r-1",
-                            active_class: "text-[var(--color-fg)] bg-[var(--color-active)] hover:bg-[var(--color-active)] hover:cursor-default text-base-light border-fg border-r-1",
-                            to: link.route.clone(),
-                            p { class: if curr_route == link.route { "scale-150" } else { "group-hover:scale-150 transition-scale duration-100" },
-                                "{link.text}"
-                            }
+                    { let is_active = check_route_active(&link.route);
+                    rsx! {
+                    Link { class: format!(
+                            "group flex items-center justify-center w-full h-full border-fg hover:bg-[var(--color-hover)] transition-scale duration-100 hover:scale-120 hover:shadow-[inset_0_0_0_1px_var(--color-fg)] hover:border-0 border-r-1 {}",
+                            if is_active { "text-[var(--color-fg)] bg-[var(--color-active)] hover:bg-[var(--color-active)] hover:cursor-default text-base-light border-fg border-r-1"} else { "" }
+                        ),
+                        active_class: "text-[var(--color-fg)] bg-[var(--color-active)] hover:bg-[var(--color-active)] hover:cursor-default text-base-light border-fg border-r-1",
+                        to: link.route.clone(),
+                        p { class: if is_active { "scale-150" } else { "group-hover:scale-150 transition-scale duration-100" },
+                            "{link.text}"
                         }
-                    }
-                }
+                    }}
+                }}
             }
         }
     }
